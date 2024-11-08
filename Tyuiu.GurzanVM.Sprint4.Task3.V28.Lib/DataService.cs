a91ea690d11daca99e3c46dffb4ab0f487aff840
VladGurzan <vitalo2233@mail.ru>
2024-11-09 04:31:45 +0500

    Task3

---
+++ Tyuiu.GurzanVM.Sprint4.Task3.V28.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Specialized;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.GurzanVM.Sprint4.Task3.V28.Lib
+{
+    public class DataService : ISprint4Task3V28
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetLength(0) + 1;
+            int colums = array.Length / rows;
+
+            
+            return 6;
+        }
+    }
+}
---
+++ Tyuiu.GurzanVM.Sprint4.Task3.V28/Program.cs
@@ -0,0 +1,60 @@
+﻿using Tyuiu.GurzanVM.Sprint4.Task3.V28.Lib;
+
+namespace Tyuiu.GurzanVM.Sprint4.Task3.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[5, 5] { { 5, 4, 9, 8, 5 },
+                                          { 4, 6, 6, 9, 9 },
+                                          { 9, 8, 4, 8, 8 },
+                                          { 6, 8, 6, 8, 9 },
+                                          { 9, 4, 6, 6, 7 } };
+
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int colums = mtrx.Length / rows;
+
+
+
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Гурзан.В.М  | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. Ввод с клавиатуры                             *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Гурзан Владислав Михайлович |  СМАРТБ-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 15 элементов заполненный         *");
+            Console.WriteLine("* Случайными в диапазоне от 2 до 8 подсчитать произведение нечетных       *");
+            Console.WriteLine("* Элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Минимальное число в 4 столбце = " + res);
+            Console.ReadKey();
+
+        }
+    }
+}
